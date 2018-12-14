0eefd171574140a5af841fe52bbe2ecfd6eeb41a
abhinavabhinavabhi <43805248+abhinavabhinavabhi@users.noreply.github.com>
2018-12-14 09:47:15 +0530

    Add files via upload

---
+++ WcfService1/WcfService1/Service1.svc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    
+    public class Service1 : IService1
+    {
+        public int getwether(string a)
+        {
+            if(a=="bangalore")
+            {
+                return (20);
+            }
+            if(a=="kerala")
+            {
+                return (30);
+            }
+            if(a=="andra pradesh")
+            {
+                return (28);
+            }
+            else
+            {
+                return (0);
+            }
+
+        }
+
+       
+    }
+}
