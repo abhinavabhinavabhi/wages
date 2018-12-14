f3fd2d08f8fb0815e47e8a8afe5fb37f570abcc6
abhinavabhinavabhi <43805248+abhinavabhinavabhi@users.noreply.github.com>
2018-12-14 09:44:49 +0530

    Add files via upload

---
+++ files/files/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace files
+{
+    public partial class Form1 : Form
+    {
+        string path = Environment.CurrentDirectory + "\\" + "sample.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            FileStream f = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite);
+            MessageBox.Show("text entered");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string txt = sr.ReadLine();
+                richTextBox1.Text = sr.ReadLine();
+            }
+        }
+    }
+}
---
+++ files/files/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* string[] name = new string[] { "abhi", "rajiv" };
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\Student\Desktop\files.txt")) 
+            {
+                foreach (string s in name)
+                {
+                    sw.WriteLine(s);
+                }
+            }
+            string line = "";
+            using (StreamReader sr = new StreamReader(@"C:\Users\Student\Desktop\files.txt"))
+            {
+                while ((line=sr.ReadLine())!=null)
+                {
+                    Console.WriteLine(line);
+                }
+                Console.ReadKey();
+            }*/
+            
+        }
+    }
+}
